ecd711b9a506dc46496510c1be26e967f68817a3
Lennart <lp@propassig.de>
2022-06-20 15:42:25 +0200

    nonsensitive( bug

--- main.tf
+++ main.tf
@@ -23,32 +23,27 @@ module "security-group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+  ingress_rules       = ["http-80-tcp"]
   egress_rules        = ["all-all"]
 
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
-  sensitive = false
+  value = nonsensitive(module.vpc.vpc_id)
 }
 
 output "subnet_id" {
-  value = element(module.vpc.public_subnets, 0)
-  sensitive = false
+  value = nonsensitive(element(module.vpc.public_subnets, 0))
 }
 
 output "availability_zone" {
-  value = element(module.vpc.azs, 0)
-  sensitive = false
+  value = nonsensitive(element(module.vpc.azs, 0))
 }
 
 output "instance_profile" {
-  value = aws_iam_instance_profile.magic_profile.name
-  sensitive = false
+  value = nonsensitive(aws_iam_instance_profile.magic_profile.name)
 }
 
 output "vpc_security_group_ids" {
-  value = [module.security-group.security_group_id]
-  sensitive = false
+  value = nonsensitive([module.security-group.security_group_id])
 }
