a16139434ff663b32215f50316ba42ec4ae89207
Lennart <lp@propassig.de>
2022-06-20 15:54:56 +0200

    no ingress :)

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "security-group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+  ingress_rules       = []
   egress_rules        = ["all-all"]
 
 }
